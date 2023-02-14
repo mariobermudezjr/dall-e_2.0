bb63bbbc68f4fd53dbecf1c6332419598977c722
Mario Bermudez JR <mario.bermudez.jr@outlook.com>
2023-02-14 17:39:16 +0700

    Add local handleChange and handleSurpriseMe functions button onClick property

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,8 +18,12 @@ const CreatePost = () => {
 
   const generateImage = () => {}
   const handleSubmit = () => {}
-  const handleChange = (e) => {}
-  const handleSurpriseMe = () => {}
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value })
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({ ...form, prompt: randomPrompt })
+  }
 
   return (
     <section className="max-q-7x1 mx-auto">
